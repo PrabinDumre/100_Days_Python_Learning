fc855f4156d38f5a3050fc0a94fb5f4a42d2b60b
PrabinDumre <116421340+PrabinDumre@users.noreply.github.com>
2024-01-05 21:07:08 +0530

    Both built-in and user define function uses

---
+++ Day-8(Functions).py
@@ -0,0 +1,17 @@
+# Built in Function
+a = 4
+b = 5
+c = sum((a,b))
+print(c)
+
+# User Define Function
+def function1(r,t):
+    print("I am Prabin")
+
+def function2(a,b):
+    """Hii  everyone"""
+    avg=(a+b)/2
+    return avg
+v = function2(7,9)
+print(v)
+print(function2.__doc__)
