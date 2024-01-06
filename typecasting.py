0d305d8665eb60bd5ee7643a98bdeb1fc6faae81
PrabinDumre <116421340+PrabinDumre@users.noreply.github.com>
2024-01-06 21:08:51 +0530

    simple code for practicing Typecasting

---
+++ typecasting.py
@@ -0,0 +1,7 @@
+var1 = "89"
+var2 = "81"
+print(var1 + var2)  #print string
+print(int(var1)+ int(var2))  # changing to integer
+print(5* "Prabin\n") # String will be multiplied to given time
+print(5 * (int(var1) + int(var2))) # First Integer will be multiplied and added to second
+print(5 * str(int(var1) + int(var2))) # First Integer will be added and converted into string and them multiplied given time
