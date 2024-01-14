3fd6df8f4a5fd2a60bbeac41188c59f743e3f4c9
PrabinDumre <116421340+PrabinDumre@users.noreply.github.com>
2024-01-14 22:02:04 +0530

    IF ELSE Statement uses

---
+++ Day-15/IF ELSE.py
@@ -0,0 +1,27 @@
+o = 24
+print("Enter a number")
+i = int(input())
+
+if i<o:  # [IF STATEMENT]
+    print("Between")
+elif i==o:  # [ELSE IF STATEMENT]
+    print("Equal")
+else:  # [ELSE STATEMENT]
+    print("greater")
+
+#  IN KEYWORD
+list = [1,2,3,4,5]
+print(5 in list)
+if 5 in list:
+    print("Yes there is")
+
+print(15 in list)
+
+
+# NOT IN KEYWORD
+print(15 not in list)
+if 15 not in list:
+    print("No there is not")
+
+
+
