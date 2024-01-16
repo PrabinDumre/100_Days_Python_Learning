514c0efd2e23aca603ef569edec182391a43d2ac
PrabinDumre <116421340+PrabinDumre@users.noreply.github.com>
2024-01-16 21:23:38 +0530

    Dictionary (Words with  Meaning)

---
+++ Day-17/Dictonary Game.py
@@ -0,0 +1,5 @@
+# Create a dictionary and take input from user and return the meaning of the word from the dictionary
+d = {"cutlet" : "A portion of meat" , "nobility" : "The quality of being nobel in character" , "specify" : "Identify clearly and definitely" , "semantic" : "Realting to mmeaning in language or logic" , "neuralnetwork" : "a computer system modelled on the human brain and nervous system"}
+print("Enter a word which meaning you want to know")
+p = input()
+print(p, "means" ,d[p])
