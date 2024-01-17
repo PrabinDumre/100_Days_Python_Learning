3c7827097232de81487bf59322f3759ab4127b32
PrabinDumre <116421340+PrabinDumre@users.noreply.github.com>
2024-01-17 22:03:47 +0530

    Learning F string

---
+++ Day-18/f string.py
@@ -0,0 +1,13 @@
+name = "Prabin"
+country = "Nepal"
+print(f"My name is {name} and I am from {country}")
+
+price = 16.0444444
+p = f"for only {price:.3f} dollars"
+print(p)
+
+# To print as it is in f string
+name = "Prabin"
+country = "Nepal"
+print(f"My name is {{name}} and I am from {{country}}")
+
