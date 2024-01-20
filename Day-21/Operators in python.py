0f8de8a88f558093b4032f97097b826880384989
PrabinDumre <116421340+PrabinDumre@users.noreply.github.com>
2024-01-20 21:40:08 +0530

    List of Operators in python

---
+++ Day-21/Operators in python.py
@@ -0,0 +1,36 @@
+#Arithmetic Operator
+print("5+6 is",5+6)
+print(5//6)
+print(5**3)
+print(5%6)
+
+# Assignment operator
+x = 5
+print(x)
+x+=7
+print(x)
+
+#Comparision Operator
+i=7
+print(i == 8)
+print(i != 8)
+
+#Logical Operator
+a = True
+b = False
+print(a and b)
+print(a or b)
+
+#Identity Operator
+print(a is not b)
+print(a is b)
+
+#Membership operator
+list = [3,4,5,7,2,3]
+print(3 in list)
+print(9 in list)
+print(0 not in list)
+
+# Bitwise Operator
+print(0 & 1)
+print(0 | 1)
