fcb05c3759c18b358fe75f5e0a4e9aadd0c0ad4b
PrabinDumre <116421340+PrabinDumre@users.noreply.github.com>
2024-02-25 21:07:04 +0530

    Mod Divmod in Python

---
+++ Day-56/Mod Divmod.py
@@ -0,0 +1,17 @@
+# Read in the two integers
+a = int(input())
+b = int(input())
+
+# Perform integer division
+integer_division = a // b
+
+# Calculate the remainder using the modulo operator
+remainder = a % b
+
+# Calculate divmod of a and b
+divmod_result = divmod(a, b)
+
+# Print the results
+print(integer_division)
+print(remainder)
+print(divmod_result)
