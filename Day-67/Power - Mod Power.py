daacc5c05c61712c682f3953c5bb311a683ad0f5
PrabinDumre <116421340+PrabinDumre@users.noreply.github.com>
2024-03-06 20:05:14 +0530

    Power - Mod Power

---
+++ Day-67/Power - Mod Power.py
@@ -0,0 +1,12 @@
+# Read input values
+a = int(input())
+b = int(input())
+m = int(input())
+
+# Calculate pow(a, b) and pow(a, b, m)
+result1 = pow(a, b)
+result2 = pow(a, b, m)
+
+# Print the results
+print(result1)
+print(result2)
