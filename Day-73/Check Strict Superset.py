21402c9a6c0e68fd7ea4dc7831a8d9a77cbcaea7
PrabinDumre <116421340+PrabinDumre@users.noreply.github.com>
2024-03-13 21:15:07 +0530

    Check Strict Superset

---
+++ Day-73/Check Strict Superset.py
@@ -0,0 +1,1 @@
+set_a = set(map(int, input().split()))n = int(input())is_strict_superset = Truefor _ in range(n):    other_set = set(map(int, input().split()))        if not (other_set.issubset(set_a) and len(other_set) < len(set_a)):        is_strict_superset = False        breakprint(is_strict_superset)
